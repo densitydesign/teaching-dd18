3e6f4373d21c4c554018dbbe23b1dd54cd027bd0
andrea <benedetti.a93@gmail.com>
2023-01-23 20:43:21 +0100

    hiding new pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav>
             <span><Link to="/">DD18</Link></span>
             <h1 className="course-title" style={{gridColumn: "span 2"}}>Final Synthesis Design Studio <br /> Sect. C3 2022/2023</h1>
-            <span style={{textAlign: 'right'}}><Link to="/posters" style={{marginRight: "1rem"}}>Posters</Link><Link to="/projects">Projects</Link></span>
+            {/* <span style={{textAlign: 'right'}}><Link to="/posters" style={{marginRight: "1rem"}}>Posters</Link><Link to="/projects">Projects</Link></span> */}
         </nav>
     )
 }
