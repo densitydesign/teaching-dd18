ed5e5e950031f4f2574802cf8f62f247167cd169
bndndr <benedetti.a93@gmail.com>
2023-01-26 17:22:41 +0100

    Fonts test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import "@fontsource/unbounded"
+import "@fontsource/karla"
--- src/pages/posters.js
+++ src/pages/posters.js
@@ -15,9 +15,9 @@ const IndexPage = () => {
   return (
     <>
     <title>Poster - Visual Explanations - DensityDesign Lab Final Synthesis Design Studio 2021/2022 - Mediazioni algoritmiche</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,800;1,400&display=swap" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,800;1,400&display=swap" rel="stylesheet"></link> */}
 
     <Navbar/>
     <main>
