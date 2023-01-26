b4e9e6a26d93a816c151044e28e80d20cd093ff3
bndndr <benedetti.a93@gmail.com>
2023-01-26 17:39:26 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,49 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
-import Navbar from "../components/navbar.js"
-import Footer from "../components/footer.js"
-import Aside from "../components/aside.js"
+import Navbar from "../components/navbar.js";
+import Footer from "../components/footer.js";
+import Aside from "../components/aside.js";
 
-import Index from "../contents/index.mdx"
+import Index from "../contents/index.mdx";
 
-import "./main.scss"
+import "./main.scss";
 
 // markup
 const IndexPage = () => {
   return (
     <>
-    <title>DensityDesign Lab Final Synthesis Design Studio 2022/2023 - Mediazioni algoritmiche</title>
-    {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,800;1,400&display=swap" rel="stylesheet"></link> */}
-    <Navbar/>
-    <div className="title-card"><h1 className="title">The <em>DensityDesign Final Synthesis Design Studio</em> focuses on learning to observe and represent controversial phenomena from different perspectives, and to visually communicate them to different publics.</h1>
-    <h2 className="subtitle">POLITECNICO DI MILANO, SCUOLA DEL DESIGN</h2></div>
-    <Aside/>
-    <main>
-      <Index />
-    </main>
-    <Footer />
+      <title>
+        DensityDesign Lab Final Synthesis Design Studio 2022/2023 - Mediazioni
+        algoritmiche
+      </title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,800;1,400&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Unbounded&display=swap"
+        rel="stylesheet"
+      />
+      <Navbar />
+      <div className="title-card">
+        <h1 className="title">
+          The <em>DensityDesign Final Synthesis Design Studio</em> focuses on
+          learning to observe and represent controversial phenomena from
+          different perspectives, and to visually communicate them to different
+          publics.
+        </h1>
+        <h2 className="subtitle">POLITECNICO DI MILANO, SCUOLA DEL DESIGN</h2>
+      </div>
+      <Aside />
+      <main>
+        <Index />
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
